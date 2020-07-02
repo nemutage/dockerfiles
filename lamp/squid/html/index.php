f535beb3e505d034efe6e5f3efb718530baf64f7
t.yamada <yamadatsukasa1105@gmail.com>
2020-07-02 21:59:00 +0900

    squid付きのLAMP環境追加

---
+++ lamp/squid/html/index.php
@@ -0,0 +1,10 @@
+<?php 
+phpinfo();
+
+/* test squid */
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, "http://www.google.com/");
+// curl_setopt($ch, CURLOPT_PROXYPORT, '3128');
+// curl_setopt($ch, CURLOPT_PROXY, 'http://squid');
+// curl_exec($ch);
+// curl_close($ch);
